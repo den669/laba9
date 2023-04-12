685b6395b5b61060483463ff0c664141dc524c79
den669 <71308629+den669@users.noreply.github.com>
2023-04-12 22:03:22 +0300

    Add files via upload

---
+++ laba9/laba9/ClassLibrary1/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+// Клас, що представляє складові гелікоптера
+public class Rotor
+{
+    public string Name { get; set; }
+
+    public Rotor(string name)
+    {
+        Name = name;
+    }
+
+    public void Rotate()
+    {
+        Console.WriteLine($"Ротор {Name} обертається.");
+    }
+}
+
+public class Engine
+{
+    public string Name { get; set; }
+
+    public Engine(string name)
+    {
+        Name = name;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine($"Двигун {Name} запущений.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine($"Двигун {Name} зупинений.");
+    }
+}
+
+public class Body
+{
+    public string Type { get; set; }
+
+    public Body(string type)
+    {
+        Type = type;
+    }
+
+    public void Assemble()
+    {
+        Console.WriteLine($"Тип корпусу: {Type}. Корпус зібраний.");
+    }
+}
+
+// Компонувальник для формування гелікоптера
+public class HelicopterComposer
+{
+    public Rotor CreateRotor(string name)
+    {
+        return new Rotor(name);
+    }
+
+    public Engine CreateEngine(string name)
+    {
+        return new Engine(name);
+    }
+
+    public Body CreateBody(string type)
+    {
+        return new Body(type);
+    }
+}
+
+
---
+++ laba9/laba9/laba9/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Створення компонувальника
+        var composer = new HelicopterComposer();
+
+        // Створення складових гелікоптера
+        var rotor1 = composer.CreateRotor("Ротор 1");
+        var rotor2 = composer.CreateRotor("Ротор 2");
+        var engine = composer.CreateEngine("Двигун 1");
+        var body = composer.CreateBody("Тип 1");
+
+        // Взаємодія зі складовими гелікоптера
+        rotor1.Rotate();
+        rotor2.Rotate();
+        engine.Start();
+        body.Assemble();
+        engine.Stop();
+
+        Console.ReadKey();
+    }
+}
